9246292ba070e9a0305944f8cf513b5ca4f2eefa
LariKMF <larissa.kirschnerfogaca@gmail.com>
2026-08-19 21:29:48 -0300

    Correcao

---
+++ Variaveis/Main.java
@@ -0,0 +1,28 @@
+package Variaveis;
+public class Main {
+    public static void main(String[] args){
+        String nome = "Savalo";
+        String sobrenome = "Horse";
+        int idade = 85;
+        double altura = 1.80;
+        boolean empregado = true;
+        String telefone = "(15) 9 9758-8790";
+        String endereco = "Rodovia Mario Batista Mori";
+        String cep = "18270-786";
+        String cidade = "Tatui";
+        String uf = "SP";
+        double salario_bruto = 1950.95;
+        double valor_hora_extra = 5.66;
+        System.out.println("Nome do aluno: " + nome+ " "+ sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Empregado: " + empregado);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereço: " + endereco);        
+        System.out.println("CEP: " + cep);
+        System.out.println("Cidade: " + cidade);
+        System.out.println("UF: " + uf);
+        System.out.println("Salario Bruto: " + salario_bruto);
+        System.out.println("Valor Hora Extra: " + valor_hora_extra);
+    }
+}
