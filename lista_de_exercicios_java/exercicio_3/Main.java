a7f42a8a9db3df7badccc07d00b4abe699a3e419
LariKMF <larissa.kirschnerfogaca@gmail.com>
2026-09-09 22:24:15 -0300

    ATUALIZACAO EXERCICIOS

--- lista_de_exercicios_java/exercicio_3/Main.java
+++ lista_de_exercicios_java/exercicio_3/Main.java
@@ -2,12 +2,12 @@ package lista_de_exercicios_java.exercicio_3;
 
 public class Main {
     public static void main(String[] args) {
-        float nota = 8;
-        if (nota >= 6); {
+        double nota = 5.9;
+        if (nota >= 6) {
             System.out.println("A nota eh " + nota + " o aluno foi APROVADO");
-        } else (nota >= 4); {
+        } else if (nota >= 4) {
             System.out.println("A nota eh " + nota + " o aluno esta de RECUPERACAO");
-        } else (nota < 4); {
+        } else {
             System.out.println("A nota eh " + nota + " o aluno foi REPROVADO");
         }
     }
